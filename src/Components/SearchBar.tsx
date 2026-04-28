076c3827bb828481595464de1458f9a465cb2db1
rathorPriyanshu <rathorepriyansh2003@gmail.com>
2026-04-29 00:57:44 +0530

    feat: implement search & filter logic

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,10 +1,15 @@
+import { usePokemonStore } from "../store/useStore";
+
 const SearchBar = () => {
+  const { search, setSearch } = usePokemonStore();
   return (
     <div>
       <input
         type="text"
         placeholder="Search Pokemon..."
         className="w-full p-2 border rounded mb-4"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
       />
     </div>
   );
--- src/Components/TypeFilter.tsx
+++ src/Components/TypeFilter.tsx
@@ -1,13 +1,20 @@
+import { usePokemonStore } from "../store/useStore";
+
 const TypeFilter = () => {
   const types = ["fire", "water", "electric", "grass"];
+  const { type, setType } = usePokemonStore();
 
   return (
     <div>
-      <select className="w-full p-2 border rounded mb-4">
+      <select
+        className="w-full p-2 border rounded mb-4"
+        value={type}
+        onChange={(e) => setType(e.target.value)}
+      >
         <option value="">All Types</option>
-        {types.map((type) => (
-          <option value={type} key={type}>
-            {type}
+        {types.map((t) => (
+          <option value={t} key={t}>
+            {t}
           </option>
         ))}
       </select>
